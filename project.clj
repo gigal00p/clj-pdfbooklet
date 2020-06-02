57cf62f248d2ac0fd7311a1fd8cbb857a6d963b8
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2020-06-02 19:07:49 +0200

    Add dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [pdfboxing "0.1.14"]
+                 [com.taoensso/timbre "4.10.0"]]
   :main ^:skip-aot clj-pdfbooklet.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
