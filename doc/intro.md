6899c8ebbfb407b89cd0f5dc63bf2038f233ee99
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2020-06-02 18:47:27 +0200

    Init lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-pdfbooklet
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_pdfbooklet/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-pdfbooklet.core-test
+  (:require [clojure.test :refer :all]
+            [clj-pdfbooklet.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
