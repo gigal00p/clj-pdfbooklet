777a11bbaea084219459d23aa2eb41717988b81d
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2020-06-02 19:07:59 +0200

    Add range function

--- src/clj_pdfbooklet/core.clj
+++ src/clj_pdfbooklet/core.clj
@@ -5,3 +5,13 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+(defn make-page-range
+  "Make vector of ints from n to m inclusive.
+   n,m needs to be positive intiger.
+   n needs to be < than m."
+  [n m]
+  {:pre  [(pos? n), (pos? m), (> m n)]
+   :post [(vector? %)]}
+  (into [] (range n (+ 1 m))))
