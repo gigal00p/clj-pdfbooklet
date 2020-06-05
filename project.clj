55f57357f8b860ec440e26418f95bd08fad85e05
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2020-06-05 17:25:45 +0200

    Remove pdfboxing from deps and go with pdfbox lib directly

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [pdfboxing "0.1.14"]
-                 [com.taoensso/timbre "4.10.0"]]
+                 [org.clojure/tools.cli "1.0.194"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [org.apache.pdfbox/pdfbox "2.0.19"]]
+  :plugins [[lein-cljfmt "0.6.7"]]
   :main ^:skip-aot clj-pdfbooklet.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
