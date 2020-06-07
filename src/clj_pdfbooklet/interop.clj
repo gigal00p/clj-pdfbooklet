faefb8392643160fcdfd18c97d3d815e6eaa4545
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2020-06-07 15:24:42 +0200

    Implement arrange-pages-side-by-side function

--- src/clj_pdfbooklet/interop.clj
+++ src/clj_pdfbooklet/interop.clj
@@ -1,10 +1,17 @@
 (ns clj-pdfbooklet.interop
   (:require [clojure.java.io :as io]
-            [taoensso.timbre :as timbre :refer [errorf]])
+            [taoensso.timbre :as timbre :refer [errorf]]
+            [clj-pdfbooklet.common :as common])
   (:import java.io.File
+           java.awt.geom.AffineTransform
            org.apache.pdfbox.pdmodel.PDDocument
            org.apache.pdfbox.pdmodel.PDPage
-           org.apache.pdfbox.multipdf.Splitter))
+           org.apache.pdfbox.multipdf.Splitter
+           org.apache.pdfbox.pdmodel.common.PDRectangle
+           org.apache.pdfbox.cos.COSDictionary
+           org.apache.pdfbox.multipdf.LayerUtility
+           org.apache.pdfbox.cos.COSName
+           org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject))
 
 (defn get-number-of-pages
   "Try get the number of pages of PDF file.
@@ -21,8 +28,8 @@
       nil)))
 
 (defn split-pdf-page
-  "Given pdf-file-or-path extracts pages from start to end and writes to
-  file-output-name. Returns true on success."
+  "Given `pdf-file-or-path` extracts pages from `start` to an `end` and
+  writes to `file-output-name`. Returns true on success."
   [pdf-file-or-path start end file-output-name]
   (try
     (let [^File       pdf-file (io/as-file pdf-file-or-path)
@@ -66,4 +73,72 @@
       true)
     (catch Exception e
       (timbre/errorf "Something went wrong while rearranging `%s` message: %s"
+                     pdf-file-in (.getMessage ^Exception e)))))
+
+
+(def pdf-in "c:/Users/walki/Sync/SHARED/books/new_books/Node-Javascript/Eloquent_JavaScript.pdf")
+(def pdf-out "c:/Users/walki/Downloads/wynik/2.pdf")
+
+
+; Below function is direct translation of solution from:
+; https://stackoverflow.com/questions/12093408/pdfbox-merge-2-portrait-pages-onto-a-single-side-by-side-landscape-page
+
+(defn arrange-pages-side-by-side
+  [pdf-file-in page-1-index page-2-index
+   pdf-file-out
+   ]
+  (try
+    (let [^File       pdf-file (io/as-file pdf-file-in)
+          ^PDDocument src-doc (PDDocument/load pdf-file)
+          ^PDDocument target-doc (PDDocument.)
+          ^PDDocument catalog (.getDocumentCatalog src-doc)
+          ^PDDocument src-pages (.getPages src-doc)
+
+          ordered-page-list [page-1-index page-2-index]
+
+          ;; extract all pages that need to be processed
+          extractet-pages (->> ordered-page-list
+                                (map #(.get src-pages %))
+                                (into []))
+
+          ;; pages to be glued together
+          ^PDPage pdf-1-page (first extractet-pages)
+          ^PDPage pdf-2-page (second extractet-pages)
+
+          ;; output PDF frames
+          ^PDRectangle pdf-1-frame (.getCropBox pdf-1-page)
+          ^PDRectangle pdf-2-frame (.getCropBox pdf-2-page)
+          ^PDRectangle out-pdf-frame (PDRectangle. (+ (.getWidth pdf-1-frame)
+                                                      (.getWidth pdf-2-frame)),
+                                                   (max (.getHeight pdf-1-frame)
+                                                        (.getHeight pdf-2-frame)))
+
+          ;; output page
+          ^COSDictionary dict (COSDictionary.)
+
+          final-dict (doto dict
+                       (.setItem COSName/TYPE, COSName/PAGE)
+                       (.setItem COSName/MEDIA_BOX, out-pdf-frame)
+                       (.setItem COSName/CROP_BOX, out-pdf-frame)
+                       (.setItem COSName/ART_BOX, out-pdf-frame))
+
+          ^PDPage out-pdf-page (PDPage. final-dict)
+          wynik (.addPage target-doc out-pdf-page)
+
+          ^LayerUtility layer-utility (LayerUtility. target-doc)
+          ^PDFormXObject form-pdf-1 (.importPageAsForm layer-utility src-doc pdf-1-page)
+          ^PDFormXObject form-pdf-2 (.importPageAsForm layer-utility src-doc pdf-2-page)
+
+          ^AffineTransform af-left (AffineTransform.)
+          left-side (.appendFormAsLayer layer-utility out-pdf-page form-pdf-1, af-left, "left")
+
+          ^AffineTransform af-right (AffineTransform/getTranslateInstance (.getWidth pdf-1-frame) 0.0)
+          right-side (.appendFormAsLayer layer-utility out-pdf-page form-pdf-2, af-right, "right")
+          ]
+      (.save target-doc (io/as-file pdf-file-out))
+      (.close target-doc)
+      (.close src-doc)
+      true)
+    (catch Exception e
+      (timbre/errorf "Something went wrong while merging pages side by side in the file `%s` message: %s"
                      pdf-file-in (.getMessage ^Exception e)))))
