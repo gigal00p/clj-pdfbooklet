db75c9f76dbada6efe631bfbfaf685dbc7859c43
Krzysztof Walkiewicz <walkiewicz_krzysztof@o2.pl>
2020-06-07 11:33:52 +0200

    Implemented rearranging pdf pages arbitrarily

--- src/clj_pdfbooklet/interop.clj
+++ src/clj_pdfbooklet/interop.clj
@@ -3,6 +3,7 @@
             [taoensso.timbre :as timbre :refer [errorf]])
   (:import java.io.File
            org.apache.pdfbox.pdmodel.PDDocument
+           org.apache.pdfbox.pdmodel.PDPage
            org.apache.pdfbox.multipdf.Splitter))
 
 (defn get-number-of-pages
@@ -24,9 +25,9 @@
   file-output-name. Returns true on success."
   [pdf-file-or-path start end file-output-name]
   (try
-    (let [^File          pdf-file (io/as-file pdf-file-or-path)
-          ^PDDocument    doc (PDDocument/load pdf-file)
-          ^Splitter      splitter (Splitter.)
+    (let [^File       pdf-file (io/as-file pdf-file-or-path)
+          ^PDDocument doc      (PDDocument/load pdf-file)
+          ^Splitter   splitter (Splitter.)
           splitter-start (.setStartPage splitter start)
           splitter-end   (.setEndPage splitter end)
           splitter-at    (.setSplitAtPage splitter end)
@@ -34,6 +35,35 @@
       (.save pd-doc (io/as-file file-output-name))
       (.close pd-doc)
       (.close doc))
-    true ; rerurn true on success
+    true ; return true on success instead of default nil
     (catch Exception e
-      (timbre/errorf "Something went wrong while splitting `%s` message: %s" pdf-file-or-path (.getMessage ^Exception e)))))
+      (timbre/errorf "Something went wrong while splitting `%s` message: %s"
+                     pdf-file-or-path (.getMessage ^Exception e)))))
+
+
+(defn rearrange-pages-pdf
+  "Given `pdf-file-in` and `ordered-page-list` of integers, rearrange
+  pages according to that list and write to `pdf-file-out`. Returns
+  `true` on success."
+  [pdf-file-in ordered-page-list pdf-file-out]
+  (try
+    (let [^File       pdf-file (io/as-file pdf-file-in)
+          ^PDDocument src-doc (PDDocument/load pdf-file)
+          ^PDDocument target-doc (PDDocument.)
+          ^PDDocument catalog (.getDocumentCatalog src-doc)
+          ^PDDocument src-pages (.getPages src-doc)
+
+          extractet-pages (->> ordered-page-list
+                               (map #(.get src-pages %)) ; extract pages in order defined in ordered-page-list
+                               (into []))
+
+          _ (doseq [page extractet-pages] ; populate new document with extracted pages
+              (.addPage target-doc page))]
+
+      (.save target-doc (io/as-file pdf-file-out))
+      (.close target-doc)
+      (.close src-doc)
+      true)
+    (catch Exception e
+      (timbre/errorf "Something went wrong while rearranging `%s` message: %s"
+                     pdf-file-in (.getMessage ^Exception e)))))
